cee3f1d8173a2401a3bb542133219ebbdfc012c2
Stella Mazzillo <stellagress5@gmail.com>
2023-05-19 11:49:41 -0400

    complete react hooks lists and keys lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,47 @@
+// import React from "react";
+
+// function NavBar() {
+//   const links = ["home", "about", "projects"];
+
+//   const mapLinks = links.map(link => { 
+//     return <a>{link}</a>
+//   })
+
+//   return <nav>{mapLinks}</nav>;
+// }
+
+// export default NavBar;
+
+
+
+
+
+
 import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const mapLinks = links.map(link => { 
+    return <a key={link} href={`#${link}`}>{link}</a>
+  })
+
+  return <nav>{mapLinks}</nav>;
 }
 
 export default NavBar;
+
+
+
+
+
+
+// import React from "react";
+
+// function NavBar() {
+//   const links = ["home", "about", "projects"];
+
+//   return <nav>{/* display an <a> tag for each link here */}</nav>;
+// }
+
+// export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,15 +1,49 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+
+  const mapTech = technologies.map(technology=> {
+    return <span key={technology}> {technology}</span>
+  })
+
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
         {/* render a <span> for each technology in the technologies array */}
+        {mapTech}
       </div>
     </div>
   );
 }
 
 export default ProjectItem;
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from "react";
+
+// function ProjectItem({ name, about, technologies }) {
+//   return (
+//     <div className="project-item">
+//       <h3>{name}</h3>
+//       <p>{about}</p>
+//       <div className="technologies">
+//         {/* render a <span> for each technology in the technologies array */}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default ProjectItem;
+
